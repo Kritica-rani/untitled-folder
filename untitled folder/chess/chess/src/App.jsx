dacceda36c7722e31dcb2a166759843fb2edc9c4
Kritica Rani <120586121+Kritica-rani@users.noreply.github.com>
2025-05-14 13:57:18 +0530

    chess update

---
+++ untitled folder/chess/chess/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { ChessBoard } from "./component/Chess";
+
+function App() {
+  return (
+    <>
+      <ChessBoard />
+    </>
+  );
+}
+
+export default App;
